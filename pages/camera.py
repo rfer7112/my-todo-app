1f4103a16b20e418acb5765322de3e01bb2b7e44
REDBLADE86 <Russell.workid@gmail.com>
2024-06-19 17:43:44 +0200

    added camera code

---
+++ pages/camera.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from PIL import Image
+
+with st.expander("Start Camera"):
+    upload_image = st.file_uploader("Upload from device")
+    camera_image = st.camera_input("Camera")
+    st.text_area("some text")
+
+if camera_image:
+    img = Image.open(camera_image)
+    gray_img = img.convert("L")
+    st.image(gray_img)
+
+if upload_image:
+    img = Image.open(upload_image)
+    gray_img = img.convert("L")
+    st.image(gray_img)
